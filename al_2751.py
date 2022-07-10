9cf02089ebd03965dcd1b89420cc636c96d9ce66
ForLunch01 <crazylunch2019@gmail.com>
2022-07-10 15:07:39 +0900

    2751번 정렬

--- al_2751.py
+++ al_2751.py
@@ -25,39 +25,40 @@ for i in range(N):
 #         quicksort(pi+1, r, nums)
 #     return nums
 
-def mergeSort(arr):
-    if len(arr) > 1:
+# def mergeSort(arr):
+#     if len(arr) > 1:
 
-        mid = len(arr)//2
-        L = arr[:mid]
-        R = arr[mid:]
+#         mid = len(arr)//2
+#         L = arr[:mid]
+#         R = arr[mid:]
  
-        mergeSort(L)
-        mergeSort(R)
+#         mergeSort(L)
+#         mergeSort(R)
  
-        i = j = k = 0
+#         i = j = k = 0
 
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
+#         while i < len(L) and j < len(R):
+#             if L[i] < R[j]:
+#                 arr[k] = L[i]
+#                 i += 1
+#             else:
+#                 arr[k] = R[j]
+#                 j += 1
+#             k += 1
 
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
+#         while i < len(L):
+#             arr[k] = L[i]
+#             i += 1
+#             k += 1
  
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
+#         while j < len(R):
+#             arr[k] = R[j]
+#             j += 1
+#             k += 1
 
 
-mergeSort(num_list)
+# mergeSort(num_list)
+num_list.sort()
 
 for i in range(N):
     print(num_list[i])
