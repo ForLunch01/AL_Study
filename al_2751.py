19d75b89a76da6fc7f06fd793f0516f7c9882f69
ForLunch01 <crazylunch2019@gmail.com>
2022-07-10 12:13:17 +0900

    알고리즘 정렬 문제 풀기

---
+++ al_2751.py
@@ -0,0 +1,63 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+num_list = []
+
+for i in range(N):
+    num_list.append(int(sys.stdin.readline()))
+
+# def partition(l, r, nums):
+#     pivot, ptr = nums[r], l
+#     for i in range(l, r):
+#         if nums[i] <= pivot:
+#             nums[i], nums[ptr] = nums[ptr], nums[i]
+#             ptr += 1
+#     nums[ptr], nums[r] = nums[r], nums[ptr]
+#     return ptr
+ 
+# def quicksort(l, r, nums):
+#     if len(nums) == 1: 
+#         return nums
+#     if l < r:
+#         pi = partition(l, r, nums)
+#         quicksort(l, pi-1, nums)  
+#         quicksort(pi+1, r, nums)
+#     return nums
+
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+ 
+        mergeSort(L)
+        mergeSort(R)
+ 
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+mergeSort(num_list)
+
+for i in range(N):
+    print(num_list[i])
