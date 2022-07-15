e6669267674cd5874f7b054731742aeef611f61b
ForLunch01 <crazylunch2019@gmail.com>
2022-07-15 16:39:00 +0900

    1620 solve

---
+++ al_1620.py
@@ -0,0 +1,32 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+p_name_list = []
+
+p_dict_name_is_key = {}
+p_dict_num_is_key = {}
+
+for i in range(N):
+    p_name_list.append(sys.stdin.readline().strip())
+
+for i in range(N):
+    p_dict_num_is_key[i+1] = p_name_list[i]
+    p_dict_name_is_key[p_name_list[i]] = i+1
+
+
+for i in range(M):
+    test = sys.stdin.readline().strip()
+    print("test", test)
+    
+    if test.isdigit():
+        print(p_dict_num_is_key[int(test)])
+
+    else:
+        print(p_dict_name_is_key[test])
+    
+
+
+        
+    
+    
