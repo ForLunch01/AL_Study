fdd824671e3e7da7cc9a228211ff620b75bfc99f
ForLunch01 <crazylunch2019@gmail.com>
2022-07-18 15:08:07 +0900

    1021 풀이 추가

--- al_1021-2.py
+++ al_1021-2.py
@@ -1,24 +1,23 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-n, m = map(int, input().split())    # 큐의 크기 n과 뽑아내려고 하는 수의 개수 m을 입력값으로 받기
-position = list(map(int, input().split()))  # 뽑아내려는 수의 위치를 입력값으로 받기
-dq = deque([i for i in range(1, n+1)])  # deque([1, 2, 3,...,n])
+n,m = map(int,input().split())
+# deque([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) 이런식으로 큐를 표현한다.
+data = deque([i for i in range(1,n+1)])
+#뽑아내려고 하는 수의 위치가 index변수에 순서대로 담아있다.
+index = list(map(int,input().split()))
 
-count = 0   # 2, 3번 수행하면 카운트 올리기
-for i in position:  # 뽑아내려는 수의 위치 하나씩 반복문 돌리기
-    while True:     # 뽑을 때까지 계속 돌리기
-        if dq[0] == i:  # dq의 첫인덱스가 뽑아내려는 수의 위치와 같다면 1번 수행
-            dq.popleft()
+count = 0
+for num in index:
+    while 1:
+        if data[0] == num:
+            data.popleft()
             break
         else:
-            if dq.index(i) < len(dq)/2:  # 뽑아내려는 수의 위치 인덱스가 dq의 길이를 반으로 나눈것보다 작을때는 왼쪽으로 움직여야 최소
-                while dq[0] != i:   # dq의 첫번째 인덱스가 i와 같아질때까지 반복
-                    dq.append(dq.popleft())  
-                    count += 1
-            else:   # 뽑아내려는 수의 위치 인덱스가 dq의 길이를 반으로 나눈것보다 클때는 오른쪽으로 움직여야 최소
-                while dq[0] != i:
-                    dq.appendleft(dq.pop())  
-                    count += 1
+            if data.index(num) <= len(data)//2:
+                data.rotate(-1)
+                count += 1
+            else:
+                data.rotate(1)
+                count += 1
+
 print(count)
--- al_1021.py
+++ al_1021.py
@@ -1,34 +1,36 @@
+# 문제 해결 방법
+# 1. N 크기를 가지는 Deque 생성
+# 2. Deque[0]의 값이 뽑으려는 수 pos_list[0]과 같으면 추출 ***매우 중요, 뽑았으면 break
+# 3. 아니라면 왼쪽 또는 오른쪽으로 Deque를 회전시켜야 함
+# 4. 이동할 거리 계산 : len(Deque) / 2 >= Deque.index[pos_list[i]] 이면 왼쪽 회전, 아니면 오른쪽 회전
+
+
 import sys
+# deque import
+from collections import deque
+
 
 N, M = map(int, sys.stdin.readline().split())
 pos_list = list(map(int, sys.stdin.readline().split()))
 
+# deque 선언
+deque_list = deque([i for i in range(1,N+1)])
 cnt = 0
 
-for m in range(M):
-    if pos_list[m] == 1:
-        N = N-1
-    else:
-        shift_num1 = pos_list[m] + 1
-        shift_num2 = N - pos_list[m] + 1
-        
-        if shift_num1 < shift_num2:
-            cnt += shift_num1
-            for i in range(M):
-                pos_list[i] = pos_list[i] - shift_num1
-                if pos_list[i] <= 0:
-                    pos_list[i] = pos_list[i] + N                        
+for i in range(M):
+    while True:
+        if deque_list[0] == pos_list[i]:
+            deque_list.popleft()
+            break
         else:
-            cnt += shift_num2
-            for i in range(M):
-                pos_list[i] = pos_list[i] + shift_num2
-                if pos_list[i] > N:
-                    pos_list[i] = pos_list[i] - N  
-                                   
-        if pos_list[m] == 1:
-            N = N-1
+            print(deque)
+            if len(deque_list)/2 >= deque_list.index(pos_list[i]):
+                deque_list.rotate(-1)
+                cnt += 1
+            else:
+                deque_list.rotate(+1)
+                cnt += 1
             
 print(cnt)
-        
 
 
