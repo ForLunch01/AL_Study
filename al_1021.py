7ce725c208d1c0c083786692e5f5832da6c1e4fb
ForLunch01 <crazylunch2019@gmail.com>
2022-07-18 14:17:12 +0900

    unsolved 1021

---
+++ al_1021.py
@@ -0,0 +1,32 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+pos_list = list(map(int, sys.stdin.readline().split()))
+
+cnt = 0
+
+for m in range(M):
+    if pos_list[m] == 1:
+        N = N-1
+    else:
+        shift_num1 = pos_list[m] - 1
+        shift_num2 = N + 1 - pos_list[m]
+        
+        if shift_num1 <= shift_num2:
+            cnt += shift_num1
+            for i in range(M):
+                pos_list[i] = pos_list[i] - shift_num1
+                if pos_list[i] <= 0:
+                    pos_list[i] = pos_list[i] + N                        
+        else:
+            cnt += shift_num2
+            for i in range(M):
+                pos_list[i] = pos_list[i] + shift_num2
+                if pos_list[i] > N:
+                    pos_list[i] = pos_list[i] - N                 
+        N = N - 1
+            
+print(cnt)
+        
+
+
