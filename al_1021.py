8755757e95a46256d14ab1b10d358da12c7b7614
ForLunch01 <crazylunch2019@gmail.com>
2022-07-18 14:39:53 +0900

    1021 풀이 추가

--- al_1021.py
+++ al_1021.py
@@ -9,10 +9,10 @@ for m in range(M):
     if pos_list[m] == 1:
         N = N-1
     else:
-        shift_num1 = pos_list[m] - 1
-        shift_num2 = N + 1 - pos_list[m]
+        shift_num1 = pos_list[m] + 1
+        shift_num2 = N - pos_list[m] + 1
         
-        if shift_num1 <= shift_num2:
+        if shift_num1 < shift_num2:
             cnt += shift_num1
             for i in range(M):
                 pos_list[i] = pos_list[i] - shift_num1
@@ -23,8 +23,10 @@ for m in range(M):
             for i in range(M):
                 pos_list[i] = pos_list[i] + shift_num2
                 if pos_list[i] > N:
-                    pos_list[i] = pos_list[i] - N                 
-        N = N - 1
+                    pos_list[i] = pos_list[i] - N  
+                                   
+        if pos_list[m] == 1:
+            N = N-1
             
 print(cnt)
         
